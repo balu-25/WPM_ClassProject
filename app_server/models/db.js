928ded10c8a84499c5c52a947ec33a3a5dccf3d0
balu-25 <pramidibalu2005@gmail.com>
2024-09-13 15:07:57 +0530

    first commit

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,36 +1,45 @@
-const mongoose = require("mongoose");
-const dbURI = "mongodb://localhost/Loc8r";
+const mongoose = require('mongoose');
+//const dbURI = 'mongodb://localhost/Loc8r';
+const dbURI ='mongodb+srv://pramidibalu2005:123@cluster0.iokkz.mongodb.net/Loc8r'
 mongoose.connect(dbURI, { useNewUrlParser: true });
-mongoose.connection.on("connected", () => {
+
+mongoose.connection.on('connected', () => {
   console.log(`Mongoose connected to ${dbURI}`);
 });
-mongoose.connection.on("error", (err) => {
+
+mongoose.connection.on('error', (err) => {
   console.log(`Mongoose connection error: ${err}`);
 });
-mongoose.connection.on("disconnected", () => {
-  console.log("Mongoose disconnected");
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
 });
+
 const gracefulShutdown = (msg, callback) => {
   mongoose.connection.close(() => {
     console.log(`Mongoose disconnected through ${msg}`);
     callback();
   });
 };
+
 // For nodemon restarts
-process.once("SIGUSR2", () => {
-  gracefulShutdown("nodemon restart", () => {
-    process.kill(process.pid, "SIGUSR2");
+process.once('SIGUSR2', () => {
+  gracefulShutdown('nodemon restart', () => {
+    process.kill(process.pid, 'SIGUSR2');
   });
 });
+
 // For app termination
-process.on("SIGINT", () => {
-  gracefulShutdown("app termination", () => {
+process.on('SIGINT', () => {
+  gracefulShutdown('app termination', () => {
     process.exit(0);
   });
 });
+
 // For Heroku app termination
-process.on("SIGTERM", () => {
-  gracefulShutdown("Heroku app shutdown", () => {
+process.on('SIGTERM', () => {
+  gracefulShutdown('Heroku app shutdown', () => {
     process.exit(0);
   });
 });
+require('.locations.js')
